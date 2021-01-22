3ef158d872fbe04b3a707cd29c982349135a9226
Sage Wentzell-Brehme <swentzel@wellesley.edu>
2021-01-22 11:33:40 -0500

    added code by JM to include age data in the survival modeling and then tested models again

--- survival_calculations_JM_Jan21.R
+++ survival_calculations_JM_Jan21.R
@@ -1,15 +1,17 @@
 # Sage Wentzell-Brehme
-# November 24, 2020
-# Senior Thesis
-# Data analysis
-# QURU sdlg survival at Hubbard Brook valley wide plots 
-# between 2011 and 2020
+# June 15, 2020
+# Summer Science Research Program
+# Initial Hubbard Brook historical Red Oak seedling survival and health data (2011-2019)
+# UPDATED Sept 8, 2020
+# Thesis data analysis - QURU sdlg survival at Hubbard Brook valley wide plots 
 
+#UPDATED Sept 30, 2020 to include 2020 data
 
 #load packages
 library(tidyverse)
 library(tidyr)
 library(ggplot2)
+library(raster)
 library(lme4)
 
 
@@ -19,11 +21,13 @@ setwd("~/Thesis/HB_quru_survival/data")
 #read in csv files
 Seed <- read_csv("HB_VW_Oak_Trans_Survival_2020_CLEAN.csv") #updated sdlg spreadsheet w/2020 data
 Seed$VW <- paste0("p", Seed$VW) # add p to beginning of VW column
+PlotCoord <- read.csv("../Plot_Table_Basic.csv")
+
+summary(Seed) #summarize the data
+table(is.na(Seed$Stat19)) # determine how many NAs are present
 
 #########################################################################
 # Data Cleaning
-#########################################################################
-
 #add PlotTag column to seed dataset to match coordinate dataset
 Seed$PlotTag <- Seed$VW
 
@@ -40,9 +44,14 @@ Seed <- Seed %>% separate(Point, c("Distance", "TransDir"), sep = -1)
 #separate and remove point type, plot off transect or on center line of transect, from distance along transect
 Seed <- Seed %>% separate(Distance, c("PointType", "Distance"), sep = 1)
 
+#################################################################
+#################################################################
+# Calculate Survival between 2018 and 2019
+#################################################################
 
 # make unique column for seedling ID
 seedIDs <- mutate(Seed, seedID = paste(VW,SdlgNum,TransDir,Distance,sep="-"))
+
 
 # test to make sure there aren't duplicates
 length(seedIDs$seedID) #1375
@@ -63,7 +72,9 @@ find_duplicates <- seedIDs %>%
 # 1. remove_dups gets row numbers (index) where duplicates & Surv 2020 = 0 (keep 1/PD)
 # 2. make duplicate rows NA in data frame and filter out NA rows
 remove_dups <- which(seedIDs$seedID %in% find_duplicates$seedID & seedIDs$Stat20 == 0)
-seedIDs[remove_dups,] <- rep(NA, ncol(seedIDs))
+d <- rep(NA, ncol(seedIDs))
+d_df <- do.call("rbind", replicate(6, d, simplify = FALSE))
+seedIDs[remove_dups,] <- d_df
 seedIDs <- filter(seedIDs, !is.na(seedID))
 
 # test to make sure there aren't duplicates again
@@ -75,11 +86,11 @@ Seed <- dplyr::select(seedIDs, -SdlgSpp, -SdlgNum, -PointType, -Distance,
                       -TransDir, -PlotDir,-Along, -TransEW, -FromTrans, -NSline)
 
 # replace PD and NF seedling status to 0
+Seed$Stat20 <- as.numeric(Seed$Stat20) # fix Stat20 class
+Seed$Stat16 <- as.numeric(Seed$Stat16) # fix Stat16 class
 Seed[Seed=="PD"] <- 0
 Seed[Seed=="NF"] <- 0
 Seed[Seed=="nf"] <- 0
-Seed$Stat20 <- as.numeric(Seed$Stat20) # fix Stat20 class
-Seed$Stat16 <- as.numeric(Seed$Stat16) # fix Stat16 class
 
 # Calculate & format the survival interval
 # 1. Select only seedID and columns with survival data
@@ -90,16 +101,16 @@ seedSurv <- dplyr::select(Seed, seedID, grep("Stat",colnames(Seed)))
 for(i in 2:(ncol(seedSurv)-1)){
   # Calculate survival over the 2-summer interval, 1 = survived, 0 = died, NA = other
   surv_interval <- ifelse(seedSurv[,i]==1 & seedSurv[,(i+1)]==1, 1, # Survived
-                          ifelse(seedSurv[,i]==0 & seedSurv[,(i+1)]==1, 0, # Died
-                                 ifelse(seedSurv[,i]==1 & is.na(seedSurv[,(i+1)]), NA, # Born
-                                        ifelse(is.na(seedSurv[,i]), NA, NA)))) # Who knows
+         ifelse(seedSurv[,i]==0 & seedSurv[,(i+1)]==1, 0, # Died
+                ifelse(seedSurv[,i]==1 & is.na(seedSurv[,(i+1)]), NA, # Born
+                       ifelse(is.na(seedSurv[,i]), NA, NA)))) # Who knows
   
   # Make data frame with seedID, interval code, survival 
   surv_df <- data.frame(seedID = seedSurv[,1], 
-                        interval = paste0("20",as.numeric(str_sub(colnames(surv_interval),5,6))-1,"-",
-                                          "20",str_sub(colnames(surv_interval),5,6)),
-                        survival = as.vector(surv_interval))
-  
+             interval = paste0("20",as.numeric(str_sub(colnames(surv_interval),5,6))-1,"-",
+                               "20",str_sub(colnames(surv_interval),5,6)),
+             survival = as.vector(surv_interval))
+
   # Build a long data frame
   if(i == 2){
     seedSurvInt <- surv_df
@@ -112,7 +123,7 @@ for(i in 2:(ncol(seedSurv)-1)){
 seedLvs <- dplyr::select(Seed, seedID, grep("Lvs",colnames(Seed))) 
 seedLvs[,2:10] <- lapply(seedLvs[,2:10], as.numeric)
 seedLvs <- pivot_longer(seedLvs, !seedID, names_to = "interval", 
-                        values_to = "leafNumber") %>%
+                    values_to = "leafNumber") %>%
   mutate(interval = paste0("20",as.numeric(str_sub(interval,4,5))-1,"-",
                            "20",str_sub(interval,4,5))) %>%
   filter(interval != "2011-2012") # leaf number started in 2012
@@ -140,7 +151,7 @@ seedBrch <- seedBrch %>%
          BrchLvD18 = LiveBrch18/(LiveBrch18+DeadBrch18)) %>%
   dplyr::select(seedID, BrchLvD20, BrchLvD19, BrchLvD18)
 seedBrch <- pivot_longer(seedBrch, !seedID, names_to = "interval", 
-                         values_to = "brchLvD") %>%
+                        values_to = "brchLvD") %>%
   mutate(interval = paste0("20",as.numeric(str_sub(interval,8,9))-1,"-",
                            "20",str_sub(interval,8,9))) 
 
@@ -149,7 +160,6 @@ seedInterval <- left_join(seedSurvInt, seedLvs) %>%
   left_join(seedDmg) %>%
   left_join(seedBrch) %>%
   filter(!is.na(survival)) # remove lines where survival is NA
-
 
 # Find the seedling age in each interval based on birth year
 seedsToAge <- unique(seedInterval$seedID)
@@ -218,15 +228,15 @@ for(s in 1:length(seedsToAge)){
 # Save data frame with cleaned and aggregated data
 write_csv(seedIntervalAge, "seedIntervalAge_tidy.csv")
 
-
 #########################################################
 #########################################################
 #Statistical Analysis
 #generalized linear models
 
 #change variable classes to factor
-seedIntervalAge$seedDamage <- as.factor(seedIntervalAge$seedDamage)
-seedIntervalAge$interval <- as.factor(seedIntervalAge$interval)
+seedInterval$seedDamage <- as.factor(seedInterval$seedDamage)
+seedInterval$interval <- as.factor(seedInterval$interval)
+
 
 #Create a model for survival by Leaf number, Damage, Live/Dead Branch ratio 
 Mod_1 <- glm(survival ~ leafNumber + seedDamage + brchLvD + yearsAlive, data=seedIntervalAge, 
@@ -238,26 +248,18 @@ RSS <- c(crossprod(Mod_1$residuals))
 MSE <- RSS / length(Mod_1$residuals)
 RMSE <- sqrt(MSE)
 
-#Create a model for survival by Leaf number, Damage, Live/Dead Branch ratio 
-Mod_1_ <- glm(survival ~ leafNumber + seedDamage + brchLvD, data=seedInterval, 
-             family = quasibinomial(logit))
-summary(Mod_1_)
-
 #Model for survival by Leaf Number
 Mod_2 <- glm(survival ~ leafNumber, data=seedInterval, family=quasibinomial(logit))
 summary(Mod_2)
 
+
 #Model for survival by Leaf Damage
 Mod_3 <- glm(survival ~ seedDamage, data=seedInterval, family=quasibinomial(logit))
 summary(Mod_3)
-TukeyHSD(aov(Mod_3)) #compare categories
 
 #Model for survival by Live/Dead Branch Ratio
 Mod_4 <- glm(survival ~ brchLvD, data=seedInterval, family=quasibinomial(logit))
 summary(Mod_4)
-
-#################################################################
-#Add in year as a random effect
 
 #Model for survival by Leaf number with Year interval as a random effect
 M1 <- glmer(survival ~ leafNumber + yearsAlive + (1|interval), 
@@ -269,39 +271,12 @@ RSS <- c(crossprod(residuals(M1))) #residual sum of squares
 MSE <- RSS / length(residuals(M1)) #mean squared error
 RMSE <- sqrt(MSE)
 
-#Model for survival by Leaf number with Year interval as a random effect
-M1_ <- glmer(survival ~ leafNumber + (1|interval), data=seedInterval, family="binomial")
-summary(M1_)
-ranef(M1_)
-#AIC = 478.6
-
-#Model for survival by Leaf number and Live/Dead Branch ratio with Year interval as a random effect
-M2 <- glmer(survival ~ leafNumber + brchLvD + (1|interval), data=seedInterval, 
-            family = "binomial")
+#Model for survival by Leaf number and Live/Dead Branch ratio 
+#with Year interval as a random effect
+M2 <- glmer(survival ~ leafNumber + brchLvD + (1|interval), 
+            data=seedIntervalAge, family = "binomial")
 summary(M2)
-ranef(M2)
-#AIC = 61.8
 
-
-#Model for survival by leaf number
-M3 <- glm(survival ~ leafNumber, data = seedInterval, family ="binomial")
-summary(M3)
-#AIC = 513
-
-#Model for survival by branch ratio with Year interval as a random effect
-M4 <- glmer(survival ~ brchLvD + (1|interval), data = seedInterval, family ="binomial")
-summary(M4)
-ranef(M4)
-#AIC = 163
-
-#Model for survival by Leaf Damage
-M5 <- glm(survival ~ seedDamage, data=seedInterval, family="binomial")
-summary(M5)
-stepAIC(M5)
-TukeyHSD(aov(M5)) # compare categories
-
-M6 <- glmer(survival ~ seedDamage + (1|interval), data=seedInterval, family="binomial")
-summary(M6)
 ###################################################
 ##Correlation tests
 
@@ -311,8 +286,3 @@ cor.test(seedInterval$leafNumber, seedInterval$brchLvD)
 
 #correlation between leaf number and survival
 cor.test(seedInterval$leafNumber, seedInterval$survival)
-#correlated p-value <0.01 and cor = 0.49
-
-#correlation between branch ratio and survival
-cor.test(seedInterval$brchLvD, seedInterval$survival)
-#correlated p-value <0.01 and cor = 0.1
